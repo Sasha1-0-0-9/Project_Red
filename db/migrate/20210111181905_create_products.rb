20b49aa2f8a093ee0d28092a6388d2c6341ec11c
Sasha <sashapysmenny@gmail.com>
2021-01-11 21:49:00 +0200

    add home page

--- db/migrate/20210111181905_create_products.rb
+++ db/migrate/20210111181905_create_products.rb
@@ -1,10 +1,11 @@
 class CreateProducts < ActiveRecord::Migration[6.1]
   def change
     create_table :products do |t|
-    	t.string :name
-    	t.integer :price
+      t.string :title
+      t.text :description
+      t.decimal :price, precision: 10, scale: 2
 
-    	t.timestamps
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,26 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_10_124627) do
+ActiveRecord::Schema.define(version: 2021_01_11_183639) do
 
   create_table "categories", force: :cascade do |t|
-    t.string "name"
+    t.string "title"
+    t.integer "order"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "products", force: :cascade do |t|
-    t.string "name"
-    t.integer "price"
+    t.string "title"
+    t.text "description"
+    t.decimal "price", precision: 10, scale: 2
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
+  add_foreign_key "products", "categories"
 end
