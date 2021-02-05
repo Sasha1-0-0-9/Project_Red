752426bca6bea16b983cf5008b85125acde4875f
Sasha <sashapysmenny@gmail.com>
2021-02-05 20:49:08 +0200

    added domain

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,5 +6,5 @@ set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :environment, "production"
 set :rails_env,   "production"
 
-set :nginx_server_name, "178.128.160.107"
+set :nginx_server_name, "project-red.redirectme.net"
 set :puma_conf, "#{shared_path}/config/puma.rb"
