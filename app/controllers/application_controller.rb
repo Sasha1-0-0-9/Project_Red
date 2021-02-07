b93fe8f989fad914387661b4576d53e2f2b3c116
Sasha <sashapysmenny@gmail.com>
2021-02-07 13:46:25 +0200

    maybe fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   helper_method :current_order
+  skip_before_action :verify_authenticity_token
 
   def current_order
     Order.find_or_create_by(user: current_user, status: :in_progress)
